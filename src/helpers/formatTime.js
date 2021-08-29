1fe2b2e4c2a3a22973a2aa89e5e38a05508f25b3
JarrodKane <kajarrod@gmail.com>
2021-08-29 22:56:31 +1000

    - Fixed the formating of time to always have 00:00:00

--- src/helpers/formatTime.js
+++ src/helpers/formatTime.js
@@ -1,4 +1,4 @@
-// Code taken from https://stackoverflow.com/questions/64112762/convert-seconds-to-minutes-and-seconds-in-mmss-format
+// Code taken from
 
 export const formatTime = (time) => {
 	let hrs = ~~(time / 3600);
@@ -7,13 +7,10 @@ export const formatTime = (time) => {
 
 	// Output like "1:01" or "4:03:59" or "123:03:59"
 	let ret = '';
-	if (hrs < 10) {
-		ret += '0' + hrs + ':' + (mins < 10 ? '0' : '');
-	} else {
-		ret += '' + hrs + ':' + (mins < 10 ? '0' : '');
-	}
 
-	ret += '' + String(mins).padStart(2, '0') + ':' + (secs < 10 ? '0' : '');
+	ret += '' + String(hrs).padStart(2, '0') + ':' + (mins < 10 ? '0' : '');
+
+	ret += '' + String(mins).padStart(1, '0') + ':' + (secs < 10 ? '0' : '');
 	ret += '' + secs;
 
 	return ret;
